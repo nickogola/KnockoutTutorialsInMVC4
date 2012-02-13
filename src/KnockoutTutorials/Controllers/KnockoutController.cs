d929c7d0d4e9773554c9f40a7492923049552ff6
Rich Czyzewski <czyzer@gmail.com>
2012-02-13 16:37:36 -0500

    01 Introduction Complete

---
+++ src/KnockoutTutorials/Controllers/KnockoutController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KnockoutTutorials.Controllers
+{
+    public class KnockoutController : Controller
+    {
+        //
+        // GET: /Knockout/
+        private List<string> _pages = new List<string>() { @"T01_Introduction" };
+
+        public ActionResult Index()
+        {
+            return View(_pages);
+        }
+
+        public ActionResult T01_Introduction()
+        {
+            return View();
+        }
+    }
+}
