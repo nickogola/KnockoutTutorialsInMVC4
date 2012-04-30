00d9c96363ca6d003b0139ef41742dc866b08cb2
Rich Czyzewski <czyzer@gmail.com>
2012-04-30 07:48:21 -0400

    Refactoring & css styling

--- src/KnockoutTutorials/Controllers/HomeController.cs
+++ src/KnockoutTutorials/Controllers/HomeController.cs
@@ -1,32 +1,82 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Script.Serialization;
+using KnockoutTutorials.Models;
 
 namespace KnockoutTutorials.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
-        {
-            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+        public ActionResult Index() { return View(); }
 
-            return View();
+        public ActionResult T01_Introduction() { return View(); }
+
+        public ActionResult T02_ListsAndCollections() { return View(); }
+
+        public ActionResult T03_SinglePageApplications() { return View(); }
+
+        public ActionResult T04_CreatingCustomBindings() { return View(); }
+
+        public ActionResult T05_LoadingAndSavingData() { return View(); }
+
+        public ActionResult MailBoxFolder(string folder)
+        {
+            folder = folder.ToLower();
+            var validFolders = new List<string> { "inbox", "archive", "sent", "spam" };
+            if (!validFolders.Contains(folder))
+                return null;
+
+            string file = "~/MailBox/Folders/" + folder + ".json";
+
+            return new ContentResult()
+            {
+                Content = System.IO.File.ReadAllText(Server.MapPath(Url.Content(file))),
+                ContentType = "application/json"
+            };
         }
 
-        public ActionResult About()
+        public ActionResult MailBoxItem(int mailId)
         {
-            ViewBag.Message = "Your quintessential app description page.";
+            if (mailId == 0) return null;
 
-            return View();
+            string file = "~/MailBox/Items/" + mailId + ".json";
+            string finalLocation = Server.MapPath(Url.Content(file));
+            //if (!System.IO.File.Exists(finalLocation)) //Download items we don't have yet
+            //    (new WebClient()).DownloadFile("http://learn.knockoutjs.com/mail?mailid=" + mailId, finalLocation);
+
+            return new ContentResult()
+            {
+                Content = System.IO.File.ReadAllText(finalLocation),
+                ContentType = "application/json"
+            };
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your quintessential contact page.";
+        private static List<Task> _tasks;
 
-            return View();
+        [HttpGet]
+        public ActionResult Tasks()
+        {
+            if (_tasks == null)
+            {
+                var javascriptSerializer = new JavaScriptSerializer();
+                _tasks = javascriptSerializer.Deserialize<List<Task>>(System.IO.File.ReadAllText(Server.MapPath(Url.Content("~/TaskData/Tasks.json"))));
+            }
+
+            //if (Request.HttpMethod == "GET")
+            return Json(_tasks, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public ActionResult Tasks(List<Task> tasks)
+        {
+            _tasks = (from t in tasks
+                      where !t._destroy
+                      select t).ToList();
+            return new HttpStatusCodeResult(HttpStatusCode.OK);
         }
     }
 }
---
+++ src/KnockoutTutorials/Models/TaskModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace KnockoutTutorials.Models
+{
+    [Serializable]
+    public class Task
+    {
+        public string title { get; set; }
+        public bool isDone { get; set; }
+        public bool _destroy { get; set; }
+    }
+}
